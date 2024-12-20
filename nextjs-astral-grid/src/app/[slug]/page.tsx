44d1c0e63ee3748d0bb7fa0228ea534a479bc42d
jtr-code <jtrtricks@gmail.com>
2024-12-20 22:45:39 +0530

    test

--- nextjs-astral-grid/src/app/[slug]/page.tsx
+++ nextjs-astral-grid/src/app/[slug]/page.tsx
@@ -20,7 +20,6 @@ export default async function PostPage({
   params: { slug: string };
 }) {
 
-  console.log("parmas", params)
 
   const post = await client.fetch<SanityDocument>(POST_QUERY, params, options);
   const postImageUrl = post.image
